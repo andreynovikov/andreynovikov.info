7d494f1eb425560eaff680abc9916ec779dae319
Andrey Novikov <novikov@gmail.com>
2024-12-13 16:41:22 +0300

    Update Pelican

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -1,22 +1,16 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
-
-#AUTHOR = 'Andrey Novikov'
+AUTHOR = 'Andrey Novikov'
 SITENAME = 'Andrey Novikov'
 SITEURL = ''
 
 PATH = 'content'
-PLUGIN_PATHS = ['pelican-plugins']
-PLUGINS = ['tag_cloud','summary','sitemap','liquid_tags.youtube']
-
-MARKDOWN = ['codehilite(css_class=highlight)','extra','del_ins']
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['tag_cloud','summary','sitemap','liquid_tags']
 
 MARKDOWN = {
     'extension_configs': {
         'markdown.extensions.codehilite': {'css_class': 'highlight'},
         'markdown.extensions.extra': {},
-        'del_ins': {},
+        'markdown_del_ins': {},
     },
 }
 
@@ -24,7 +18,7 @@ TIMEZONE = 'Europe/Moscow'
 
 DEFAULT_LANG = 'ru'
 
-STATIC_PATHS = ['cars','gis','travel']
+STATIC_PATHS = ['cars', 'gis','travel']
 
 SUMMARY_MAX_LENGTH = 120
 
@@ -36,38 +30,45 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('MapTrek', 'http://maptrek.mobi'),
-         ('Androzic', 'http://androzic.com/'),
-         ('Xpoint', 'http://xpoint.ru/user/1'),
-         ('ФРИ', 'http://ezhe.ru/fri/527/'),)
+LINKS = (
+    ('MapTrek', 'http://maptrek.mobi'),
+    ('Androzic', 'http://androzic.com/'),
+    ('Xpoint', 'http://xpoint.ru/user/1'),
+    ('ФРИ', 'http://ezhe.ru/fri/527/'),
+)
 
 # Social widget
-SOCIAL = (('google-plus', 'https://plus.google.com/100618994892995632809'),
-          ('facebook', 'http://www.facebook.com/andrey.g.novikov'),
-          ('twitter', 'http://twitter.com/andreynovikov'),
-          ('youtube', 'http://www.youtube.com/profile?user=novikovandrey'),
-          ('flickr', 'http://www.flickr.com/photos/andreynovikov/'),
-          #('panoramio', 'http://www.panoramio.com/user/725010'),
-          ('github', 'http://github.com/andreynovikov'),
-          ('stack-overflow', 'http://stackoverflow.com/users/488489/andrey-novikov'),)
+SOCIAL = (
+    ('google-plus', 'https://plus.google.com/100618994892995632809'),
+    ('facebook', 'http://www.facebook.com/andrey.g.novikov'),
+    ('twitter', 'http://twitter.com/andreynovikov'),
+    ('youtube', 'http://www.youtube.com/profile?user=novikovandrey'),
+    ('flickr', 'http://www.flickr.com/photos/andreynovikov/'),
+    #('panoramio', 'http://www.panoramio.com/user/725010'),
+    ('github', 'http://github.com/andreynovikov'),
+    ('stack-overflow', 'http://stackoverflow.com/users/488489/andrey-novikov'),
+)
 
 DEFAULT_PAGINATION = 5
 
-SITEMAP = {'format': 'xml',
-           'priorities': {
-               'articles': 0.3,
-               'indexes': 0.5,
-               'pages': 1.0
-           },
-           'changefreqs': {
-               'articles': 'monthly',
-               'indexes': 'weekly',
-               'pages': 'yearly'
-           }
-}
-
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
+
+LIQUID_TAGS = ['youtube']
+
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.3,
+        'indexes': 0.5,
+        'pages': 1.0
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'weekly',
+        'pages': 'yearly'
+    }
+}
 
 THEME = 'html5up-striped'
 
--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -1,25 +1,31 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
-
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
 
 import os
 import sys
+
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://andreynovikov.info'
+# If your site is available via HTTPS, make sure SITEURL begins with https://
+SITEURL = "https://andreynovikov.info"
 RELATIVE_URLS = False
 
-# FEED_ALL_ATOM = 'feeds/all.atom.xml'
-# CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+# FEED_ALL_ATOM = "feeds/all.atom.xml"
+# CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-DISQUS_SITENAME = 'andreynovikov-info'
-GOOGLE_ANALYTICS = 'UA-72552438-1'
-GOOGLE_SITE_VERIFICATION = 'Kpb-e90rCCQ-TI7guNhsEwANcizR07Hd2ddV7zvEn9Q'
+DISQUS_SITENAME = "andreynovikov-info"
+ANALYTICS = """
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HVMCJLW9LT"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-HVMCJLW9LT');
+</script>
+"""
